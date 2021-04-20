83a6be83f6273dbf62d6d7e11ade7d09252e181f
Valex998 <73337937+Valex998@users.noreply.github.com>
2021-04-20 21:58:15 +0300

    Initial
    Only input

---
+++ GoogleBlocksInterview.py
@@ -0,0 +1,6 @@
+blocks = ({"gym" : 0, "school" : 1, "store" : 0},
+          {"gym" : 1, "school" : 0, "store" : 0},
+          {"gym" : 0, "school" : 1, "store" : 0},
+          {"gym" : 0, "school" : 1, "store" : 1})
+
+priority = ["gym", "school" , "store"]
